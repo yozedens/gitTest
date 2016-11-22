0903948c06ef8c17a1de349f340418023ac3f269
yozedens <zdyumath@gmail.com>
2016-11-22 09:17:53 +0800

    git add gitTest.cpp

---
+++ gitText/gitTest/gitTest.cpp
@@ -0,0 +1,11 @@
+#include"gitTest.h"
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << " Hello YZD" << endl;
+	system("pause");
+	return 0;
+}
---
+++ gitText/gitTest/gitTest.h
@@ -0,0 +1,2 @@
+#pragma once
+const int N = 10;
